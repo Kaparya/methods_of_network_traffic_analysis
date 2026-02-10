10662a3b7062a699efb86fd8ce94355fbf99caf6
Kaparya <kaparyaka.dev@gmail.com>
2026-02-10 09:14:54 +0300

    Try 1

--- classification/src/pipeline.py
+++ classification/src/pipeline.py
@@ -1,12 +1,16 @@
 from src.core import Handler
-from src.handlers.io import LoadCSVHandler, SaveDataHandler
+from src.handlers.io import LoadCSVHandler
+from src.handlers.filtering import FilterITRolesHandler
+from src.handlers.labeling import LabelGradeHandler
 from src.handlers.parsing import (
-    ParseGenderAgeBirthdayHandler, ParseSalaryHandler, ParseJobHandler,
+    ParseGenderAgeBirthdayHandler, ParseSalaryHandler,
     ParseCityHandler, ParseEmploymentHandler, ParseWorkScheduleHandler,
-    ParseExperienceHandler, ParseLastPlaceHandler, ParseLastJobHandler,
-    ParseEducationHandler, ParseResumeHandler, ParseAutoHandler
+    ParseExperienceHandler, ParseLastPlaceHandler,
+    ParseEducationHandler, ParseResumeHandler, ParseAutoHandler, ParseEperienceNLPHandler
 )
-from src.handlers.preprocessing import EncodeCategoricalFeaturesHandler, SplitDataHandler
+from src.handlers.preprocessing import (
+    EncodeCategoricalFeaturesHandler, SplitClassificationDataHandler
+)
 
 def build_pipeline() -> Handler:
     """
@@ -16,38 +20,37 @@ def build_pipeline() -> Handler:
         Handler: The first handler in the pipeline (LoadCSVHandler).
     """
     load = LoadCSVHandler()
+    filter = FilterITRolesHandler()
     gender_age = ParseGenderAgeBirthdayHandler()
     salary = ParseSalaryHandler()
-    job = ParseJobHandler()
     city = ParseCityHandler()
     employment = ParseEmploymentHandler()
     work_schedule = ParseWorkScheduleHandler()
     experience = ParseExperienceHandler()
+    experienceNLP = ParseEperienceNLPHandler()
+    grade = LabelGradeHandler()
     last_place = ParseLastPlaceHandler()
-    last_job = ParseLastJobHandler()
     education = ParseEducationHandler()
     resume = ParseResumeHandler()
     auto = ParseAutoHandler()
     encode_categorical_features = EncodeCategoricalFeaturesHandler()
 
-    split_data = SplitDataHandler()
+    split_data = SplitClassificationDataHandler()
 
-    save_data = SaveDataHandler()
-
-    load.set_next(gender_age)\
+    load.set_next(filter)\
+        .set_next(gender_age)\
         .set_next(salary)\
-        .set_next(job)\
+        .set_next(experience)\
+        .set_next(experienceNLP)\
+        .set_next(grade)\
         .set_next(city)\
         .set_next(employment)\
         .set_next(work_schedule)\
-        .set_next(experience)\
         .set_next(last_place)\
-        .set_next(last_job)\
         .set_next(education)\
         .set_next(resume)\
         .set_next(auto)\
         .set_next(encode_categorical_features)\
-        .set_next(split_data)\
-        .set_next(save_data)
+        .set_next(split_data)
     
     return load
